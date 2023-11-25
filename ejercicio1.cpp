29fbd763aab32c18d310f713532c5b304f0de09e
ALEXBETANCOUR <luis.betancourt.santos@utelvt.edu.ec>
2023-11-24 22:03:28 -0500

    nuevo

---
+++ ejercicio1.cpp
@@ -0,0 +1,17 @@
+// Programa: Sumas
+// Autor: Betancour Alex
+// Fecha: 24-11-23
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int number1, number2, sume;
+    cout << "Ingresa el primer numero entero: ";
+    cin >> number1;
+    cout << "Ingresa el segundo numero entero: ";
+    cin >> number2;
+    sume = number1 + number2;
+    cout << "La suma sera de " << number1 << " y " << number2 << " es: " << sume << endl;
+    return 0;
+}
